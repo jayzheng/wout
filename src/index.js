bd67d6147efe67feb7888fb0f2258ce1257eb551
jay <jayzheng07@gmail.com>
2019-09-07 23:38:04 -0400

    use header and footer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Hello from './Hello'
-
+import App from "./components/App";
 import "./styles.css";
-
-function App() {
-  return (
-    <div className="App">
-      <Hello name="Joe" />
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
